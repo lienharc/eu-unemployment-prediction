f43497a051a774d2bb45e2fa71e335bc5f1daff3
Christoph Lienhard <christoph.lienhard@gmx.de>
2023-09-10 14:11:36 +0200

    InputDataType: add png name property

--- src/eu_unemployment_prediction/input_data_type.py
+++ src/eu_unemployment_prediction/input_data_type.py
@@ -2,15 +2,19 @@ from enum import Enum
 
 
 class InputDataType(Enum):
-    UNEMPLOYMENT = "unemployment_seasonadjusted.csv"
-    DOLLAR_EURO_EXCHANGE_RATE = "dollar_euro_exchange_rate.csv"
-    GDP = "gdp_at_market_price.csv"
-    GOV_DEBT = "government_debt.csv"
-    INFLATION_RATE = "inflation_rate.csv"
-    LABOUR_PRODUCTIVITY = "labour_productivity.csv"
-    MONETARY_AGGREGATE_M3 = "monetary_aggregate_m3.csv"
-    POPULATION = "population.csv"
+    UNEMPLOYMENT = "unemployment_seasonadjusted"
+    DOLLAR_EURO_EXCHANGE_RATE = "dollar_euro_exchange_rate"
+    GDP = "gdp_at_market_price"
+    GOV_DEBT = "government_debt"
+    INFLATION_RATE = "inflation_rate"
+    LABOUR_PRODUCTIVITY = "labour_productivity"
+    MONETARY_AGGREGATE_M3 = "monetary_aggregate_m3"
+    POPULATION = "population"
 
     @property
     def default_file_name(self) -> str:
-        return self.value
+        return self.value + ".csv"
+
+    @property
+    def png_name(self) -> str:
+        return self.value + ".png"
