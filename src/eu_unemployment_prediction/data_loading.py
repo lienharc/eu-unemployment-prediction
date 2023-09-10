3ff3fa43097a7b7fefbcc5c544d98a1399715fb9
Christoph Lienhard <christoph.lienhard@gmx.de>
2023-09-10 13:29:32 +0200

    Remove unnecessary argument in load_unemployment_data

--- src/eu_unemployment_prediction/data_loading.py
+++ src/eu_unemployment_prediction/data_loading.py
@@ -13,7 +13,6 @@ def load_unemployment_data(file_path: Path) -> pd.Series:  # type: ignore
         names=["date", "unemployment rate"],
         usecols=[0, 1],
         index_col=0,
-        parse_dates=[0],
         date_format="%Y%b",
     ).squeeze()
 
