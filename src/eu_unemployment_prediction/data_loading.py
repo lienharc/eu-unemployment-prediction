8fca1124d47e4a24bb1651eb2154a36e157cf9a6
Christoph Lienhard <christoph.lienhard@gmx.de>
2023-09-10 11:58:45 +0200

    Add $-EUR-exchange rate loader

--- src/eu_unemployment_prediction/data_loading.py
+++ src/eu_unemployment_prediction/data_loading.py
@@ -1,5 +1,3 @@
-from __future__ import annotations
-
 from pathlib import Path
 
 import pandas as pd
@@ -15,4 +13,17 @@ def load_unemployment_data(file_path: Path) -> pd.Series:  # type: ignore
         index_col=0,
         parse_dates=[0],
         date_format="%Y%b",
+    ).squeeze()
+
+
+# Type is ignored since mypy won't take pd.Series and intellij won't take pd.Series[float]
+def load_dollar_euro_exchange_rate(file_path: Path) -> pd.Series:  # type: ignore
+    return pd.read_csv(  # type: ignore
+        file_path,
+        header=5,
+        names=["date", "exchange rate"],
+        usecols=[0, 1],
+        index_col=0,
+        parse_dates=[0],
+        na_values={"exchange rate": "-"},
     ).squeeze()
--- test/eu_unemployment_prediction/data_loading/test_data_loading.py
+++ test/eu_unemployment_prediction/data_loading/test_data_loading.py
@@ -1,9 +1,10 @@
+import math
 from pathlib import Path
 
 import pandas as pd
 from pandas import Timestamp
 
-from eu_unemployment_prediction.data_loading import load_unemployment_data
+from eu_unemployment_prediction.data_loading import load_unemployment_data, load_dollar_euro_exchange_rate
 
 
 def test_load_unemployment_data() -> None:
@@ -17,3 +18,17 @@ def test_load_unemployment_data() -> None:
     assert output_df.index[-1] == Timestamp.fromisoformat("2000-01-01T00:00")
     assert output_df[-1] == 9.43814
     assert output_df.shape == (283,)
+
+
+def test_load_dollar_euro_exchange_rate() -> None:
+    input_file = Path(__file__).parent.parent.parent.parent / "data" / "dollar_euro_exchange_rate.csv"
+
+    output_df = load_dollar_euro_exchange_rate(input_file)
+
+    assert type(output_df) == pd.Series
+    assert output_df.index[0] == Timestamp.fromisoformat("2023-09-08T00:00")
+    assert output_df[0] == 1.0704
+    assert output_df.index[-1] == Timestamp.fromisoformat("1999-01-04T00:00")
+    assert output_df[-1] == 1.1789
+    assert math.isnan(output_df.at["2012-04-09"])
+    assert output_df.shape == (6386,)
