3e4321f11b0a20101a451244f23f7706ac3f47d5
Christoph Lienhard <christoph.lienhard@gmx.de>
2023-09-10 13:39:23 +0200

    Add loading function for m3 data

--- src/eu_unemployment_prediction/data_loading.py
+++ src/eu_unemployment_prediction/data_loading.py
@@ -85,3 +85,17 @@ def load_labour_productivity(file_path: Path) -> pd.Series:  # type: ignore
     )
     data_frame.index = data_frame.index.map(convert_quarterly_format_to_date)
     return data_frame.squeeze()  # type: ignore
+
+
+# Type is ignored since mypy won't take pd.Series and intellij won't take pd.Series[float]
+def load_monetary_aggregate_m3(file_path: Path) -> pd.Series:  # type: ignore
+    column_name = "m3"
+    data_frame = pd.read_csv(
+        file_path,
+        header=5,
+        names=["date", column_name],
+        usecols=[0, 1],
+        index_col=0,
+        date_format="%Y%b",
+    )
+    return data_frame.squeeze()  # type: ignore
