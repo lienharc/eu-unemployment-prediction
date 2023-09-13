834c70939d7ec8a1fb02da78cc3b67f16351b2a3
Christoph Lienhard <christoph.lienhard@gmx.de>
2023-09-13 22:41:21 +0200

    Add method to normalize data to input_data_type enum

--- src/eu_unemployment_prediction/input_data_type.py
+++ src/eu_unemployment_prediction/input_data_type.py
@@ -1,16 +1,29 @@
 from enum import Enum
+from typing import Callable
+
+import numpy as np
+import numpy.typing as npt
+import pandas as pd
+
+
+def _not_implemented_yet(x: npt.NDArray[np.float32]) -> npt.NDArray[np.float32]:
+    raise NotImplementedError("Conversion function hasn't been implemented for this input data type")
+
+
+def _normalize_percentage_rate(x: npt.NDArray[np.float32]) -> npt.NDArray[np.float32]:
+    return x / 100.0
 
 
 class InputDataType(Enum):
-    UNEMPLOYMENT = ("unemployment_seasonadjusted", "unemployment rate")
-    DOLLAR_EURO_EXCHANGE_RATE = ("dollar_euro_exchange_rate", "exchange rate")
-    GDP = ("gdp_at_market_price", "gdp at market price")
-    GOV_DEBT = ("government_debt", "government debt")
-    INFLATION_RATE = ("inflation_rate", "inflation rate")
-    LABOUR_PRODUCTIVITY = ("labour_productivity", "labour productivity")
-    MONETARY_AGGREGATE_M3 = ("monetary_aggregate_m3", "m3")
-    POPULATION = ("population", "population")
-    LABOUR_COSTS = ("unit_labour_costs", "labour costs")
+    UNEMPLOYMENT = ("unemployment_seasonadjusted", "unemployment rate", _normalize_percentage_rate)
+    DOLLAR_EURO_EXCHANGE_RATE = ("dollar_euro_exchange_rate", "exchange rate", _not_implemented_yet)
+    GDP = ("gdp_at_market_price", "gdp at market price", _not_implemented_yet)
+    GOV_DEBT = ("government_debt", "government debt", _not_implemented_yet)
+    INFLATION_RATE = ("inflation_rate", "inflation rate", _not_implemented_yet)
+    LABOUR_PRODUCTIVITY = ("labour_productivity", "labour productivity", _not_implemented_yet)
+    MONETARY_AGGREGATE_M3 = ("monetary_aggregate_m3", "m3", _not_implemented_yet)
+    POPULATION = ("population", "population", _not_implemented_yet)
+    LABOUR_COSTS = ("unit_labour_costs", "labour costs", _not_implemented_yet)
 
     @property
     def file_base_name(self) -> str:
@@ -21,9 +34,22 @@ class InputDataType(Enum):
         return self.value[1]
 
     @property
+    def normalize_function(self) -> Callable[[npt.NDArray[np.float32]], npt.NDArray[np.float32]]:
+        return self.value[2]
+
+    @property
+    def normalized_column_name(self) -> str:
+        return self.column_name + " norm"
+
+    @property
     def default_file_name(self) -> str:
         return self.value[0] + ".csv"
 
     @property
     def png_name(self) -> str:
         return self.value[0] + ".png"
+
+    def add_normalized_column(self, data_frame: pd.DataFrame) -> pd.DataFrame:
+        new_column = data_frame.loc[:, self.column_name].to_numpy(dtype=np.float32)
+        data_frame[self.normalized_column_name] = self.normalize_function(new_column)
+        return data_frame
---
+++ test/eu_unemployment_prediction/test_input_data_type.py
@@ -0,0 +1,37 @@
+import math
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+
+from eu_unemployment_prediction.data_loading import load_unemployment_data
+from eu_unemployment_prediction.input_data_type import InputDataType
+
+DATA_DIR = Path(__file__).parent.parent.parent / "data"
+
+
+def test_add_normalized_column_with_default_data_set() -> None:
+    data_set = load_unemployment_data(DATA_DIR).to_frame()
+
+    data_set_with_normalized_column = InputDataType.UNEMPLOYMENT.add_normalized_column(data_set)
+
+    new_column = data_set_with_normalized_column.loc[:, InputDataType.UNEMPLOYMENT.normalized_column_name].to_numpy()
+    np.testing.assert_almost_equal(
+        new_column * 100.0, data_set[InputDataType.UNEMPLOYMENT.column_name].to_numpy(), decimal=6
+    )
+
+
+def test_add_normalized_column_if_data_missing_for_index() -> None:
+    data_set = pd.DataFrame(
+        {InputDataType.UNEMPLOYMENT.column_name: [0.0, 1.0, 2.0]},
+        index=[pd.Timestamp(f"2020-01-0{i}") for i in range(1, 4)],
+    )
+    nan_index_value = "2020-01-31"
+    data_set.loc[pd.Timestamp(nan_index_value), "another column"] = 13.0
+
+    data_set_with_normalized_column = InputDataType.UNEMPLOYMENT.add_normalized_column(data_set)
+
+    normalized_nan_cell = data_set_with_normalized_column.loc[
+        nan_index_value, InputDataType.UNEMPLOYMENT.normalized_column_name
+    ]
+    np.testing.assert_equal(normalized_nan_cell, np.nan)
