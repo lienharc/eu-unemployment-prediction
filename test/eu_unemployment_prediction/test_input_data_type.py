dcbfa1b5c2dae807608b484da2c0a20669962448
Christoph Lienhard <christoph.lienhard@gmx.de>
2023-09-15 01:34:44 +0200

    refactor: Add pytest fixture for data_dir

--- test/eu_unemployment_prediction/test_input_data_type.py
+++ test/eu_unemployment_prediction/test_input_data_type.py
@@ -1,15 +1,12 @@
 from pathlib import Path
 
 import numpy as np
-import pandas as pd
 
 from eu_unemployment_prediction.input_data_type import InputDataType
 
-DATA_DIR = Path(__file__).parent.parent.parent / "data"
 
-
-def test_add_normalized_column_with_default_data_set() -> None:
-    data_set_with_normalized_column = InputDataType.UNEMPLOYMENT.load_with_normalized_column(DATA_DIR)
+def test_add_normalized_column_with_default_data_set(data_dir: Path) -> None:
+    data_set_with_normalized_column = InputDataType.UNEMPLOYMENT.load_with_normalized_column(data_dir)
 
     new_column = data_set_with_normalized_column.loc[:, InputDataType.UNEMPLOYMENT.normalized_column_name].to_numpy()
     original_column = data_set_with_normalized_column.loc[:, InputDataType.UNEMPLOYMENT.column_name].to_numpy()
