f984c68f7227b82d2529483f74266e1c95ef93d6
Christoph Lienhard <christoph.lienhard@tngtech.com>
2023-09-27 15:01:11 +0200

    LSTM scripts: Add additional logging StreamHandler for all case

--- scripts/lstm_all.py
+++ scripts/lstm_all.py
@@ -4,7 +4,10 @@ from base_lstm import train_and_plot  # type: ignore
 from eu_unemployment_prediction.input_data_type import InputDataType
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, filename="lstm_all.log")
+    logging.basicConfig(
+        level=logging.INFO,
+        handlers=[logging.StreamHandler(), logging.FileHandler("lstm_all.log")],
+    )
     train_and_plot(
         # exclude POPULATION because it's latest data point is the end of 2022
         [feature for feature in InputDataType if feature is not InputDataType.POPULATION],
